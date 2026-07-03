cdf1502a161f211aecdc9ab57069b52d5fb0fbdb
Abdullah746-Hfs <abdullahvistro2009@gmail.com>
2026-07-03 14:29:02 +0500

    Add files via upload
    Initial upload of my Python practice files

---
+++ Abdullah (1).py
@@ -0,0 +1,15 @@
+#Taking name as input
+name = input("Please enter your name")
+print()
+print("Your name=", name)
+
+#Welcome
+print("Welcome", name)
+
+#Taking age as input
+age = input("Please enter your age")
+print("thankyou for entering your age")
+
+
+
+
---
+++ random practise.py
@@ -0,0 +1,3 @@
+name = input("enter")
+print("name in u case", name.upper())
+print("length is", len(name))
